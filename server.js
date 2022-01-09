81ceebf1016c16a495da28e57bf962f8a560734f
aaazzz <akrm@hey.com>
2022-01-09 11:51:08 +0700

    fix missing broadcast

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ io.on('connection', (socket) => {
     console.log('roomId=', roomId)
     console.log('userId=', userId)
     socket.join(roomId)
-    socket.to(roomId).emit('user-connected', userId)
+    socket.broadcast.to(roomId).emit('user-connected', userId)
     socket.on('disconnect', () => {
-      socket.to(roomId).emit('user-disconnected', userId)
+      socket.broadcast.to(roomId).emit('user-disconnected', userId)
     })
   })
 })
